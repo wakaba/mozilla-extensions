abb8ac6964e733360cf2f49960c8299ac18b1de9
Wakaba <w@suika.fam.cx>
2003-07-06 08:32:28 +0000

    Locale support

--- sidebar/navlinksidebar/navlinksidebar.jarsrc/content/sidebar.js
+++ sidebar/navlinksidebar/navlinksidebar.jarsrc/content/sidebar.js
@@ -1,13 +1,8 @@
-<?xml version="1.0" encoding="utf-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title xml:lang="en">Navigation</title>
-<link rel="stylesheet" href="sidebar.css" type="text/css" />
-<script type="application/x-javascript">
+function init() {
   const kObserverServiceIID  = "@mozilla.org/observer-service;1";
   var NavLoadObserver = {
     observe: function(aWindow) {
-      if (document.getElementById('pageFixed').checked) return(true);
+      if (document.getElementById('pageFixed').checked) returnv (true);
       var hLinks = aWindow.document.getElementsByTagName('link');
       var rURI;
       var linkTypes = ["sidebar navigator", "navigator", "contents", "toc", "index", "start", "top", "home"];
@@ -55,7 +50,7 @@
           },true);
         }
       }
-      return;
+      return true;
     }
   };
   var observerService = Components.classes[kObserverServiceIID].getService(Components.interfaces["nsIObserverService"]);
@@ -63,46 +58,69 @@
   window.addEventListener("unload", function () {
     observerService.removeObserver(NavLoadObserver, "EndDocumentLoad", false);
   }, false);
-</script>
-</head>
-<body>
-<div id="option"><label xml:lang="ja"><input type="checkbox" value="on" id="pageFixed" />固定</label></div>
-<div id="navigation"><iframe id="navBrowser" class="navBrowser" /></div>
-</body>
-</html>
-<!--
-***** BEGIN LICENSE BLOCK *****
-Version: MPL 1.1/GPL 2.0/LGPL 2.1
+}
 
-The contents of this file are subject to the Mozilla Public License Version
-1.1 (the "License"); you may not use this file except in compliance with
-the License. You may obtain a copy of the License at 
-<http://www.mozilla.org/MPL/>
+/* Taken from Content Holder <http://white.sakura.ne.jp/~piro/xul/_contentholder.html>
+   code */
+var contentHolderDNDObserver =
+{
+	onDrop: function (aEvent, aXferData, aDragSession)
+	{
+		aEvent.preventDefault();
+		aEvent.preventBubble();
 
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-for the specific language governing rights and limitations under the
-License.
+		// "window.retrieveURLFromData()" is old implementation
+		var url = 'retrieveURLFromData' in window ? retrieveURLFromData(aXferData.data, aXferData.flavour.contentType) : transferUtils.retrieveURLFromData(aXferData.data, aXferData.flavour.contentType) ;
+		if (!url || !url.length || url.indexOf(' ', 0) != -1)
+			return;
+		
+		document.getElementById('navBrowser').contentWindow.location.href = url;
+	},
 
-The Initial Developer is Wakaba <w@suika.fam.cx>.
-Portions created by the Initial Developer are Copyright (C) 2003
-the Initial Developer. All Rights Reserved.
+	getSupportedFlavours: function ()
+	{
+		var flavourSet = new FlavourSet();
+		flavourSet.appendFlavour('text/x-moz-url');
+		flavourSet.appendFlavour('text/unicode');
+		flavourSet.appendFlavour('application/x-moz-file', 'nsIFile');
+		return flavourSet;
+	}
+};
 
-Contributor(s):
-	Wakaba <w@suika.fam.cx> (original author)
-
-Alternatively, the contents of this file may be used under the terms of
-either the GNU General Public License Version 2 or later (the "GPL"), or
-the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-in which case the provisions of the GPL or the LGPL are applicable instead
-of those above. If you wish to allow use of your version of this file only
-under the terms of either the GPL or the LGPL, and not to allow others to
-use your version of this file under the terms of the MPL, indicate your
-decision by deleting the provisions above and replace them with the notice
-and other provisions required by the GPL or the LGPL. If you do not delete
-the provisions above, a recipient may use your version of this file under
-the terms of any one of the MPL, the GPL or the LGPL.
-
-***** END LICENSE BLOCK *****
--->
-<!-- $Date: 2003/07/06 02:03:41 $ -->
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is navLinkSidebar code.
+ *
+ * The Initial Developer of the Original Code is 
+ * Wakaba <w@suika.fam.cx>.
+ * Portions created by the Initial Developer are Copyright (C) 2003
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Wakaba <w@suika.fam.cx>
+ *
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the NPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the NPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
---
+++ sidebar/navlinksidebar/replace.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/perl
+use strict;
+require Getopt::Long;
+my %opt = (type => 'js');
+Getopt::Long::GetOptions (
+  'input=s'	=> \$opt{input},
+  'output-type=s'	=> \$opt{type},
+);
+
+my %var = (percent => '%');
+my $name;
+while (<>) {
+  if (/^(.+?)(?:\[([^]]+)\])?:$/) {
+    $name = $1;
+    $var{$name}->{type} = $2;
+  } elsif (/^	(.*)/) {
+    my $s = replace_percent ($1, \%var);
+    if ($var{$name}->{type} eq 'list') {
+      push @{$var{$name}->{value}}, $s;
+    } else {
+      $var{$name}->{value} .= "\n" if defined $var{$name}->{value};
+      $var{$name}->{value} .= $s;
+    }
+  }
+}
+
+open SRC, $opt{input} or die "$0: $opt{input}: $!";
+  print scalar commentize (qq(This file is auto-generated (at @{[
+                              sprintf '%04d-%02d-%02dT%02d:%02d:%02dZ',
+                                      (gmtime)[5]+1900, (gmtime)[4]+1, (gmtime)[3,2,1,0]
+                              ]}).\n)
+                          .qq(Do not edit by hand!\n))
+        unless $opt{type} eq 'xml';
+  while (<SRC>) {
+    print scalar replace_percent ($_, \%var);
+  }
+close SRC;
+
+exit;
+
+sub replace_percent ($$) {
+  my ($s, $l) = @_;
+  $s =~ s{%%([^%]+)%%}{
+    my ($r, $type) = _get_replacement_text ($1, $l);
+    if ($type eq 'list') {
+      $r = '"' . join ('", "', @$r) . '"';
+    }
+    $r;
+  }ge;
+  $s;
+}
+
+sub _get_replacement_text ($$) {
+  my ($n, $l) = @_;
+  my ($rm, $type) = ($l->{$n}->{value}, $l->{$n}->{type});
+  unless (defined $rm) {
+    if ($n eq 'current-date-time') {
+      $rm = sprintf '%04d-%02d-%02dT%02d:%02d:%02dZ',
+                    (gmtime)[5]+1900, (gmtime)[4]+1, (gmtime)[3,2,1,0]
+    }
+  }
+  ($rm, $type);
+}
+
+sub commentize ($) {
+  my $s = shift;
+  if ($opt{type} eq 'js') {
+    $s =~ s!^! * !mg;
+    return "/*\n" . $s . " */\n";
+  } elsif ($opt{type} eq 'xml') {
+    $s =~ s!^!   - !mg;
+    return "<!--\n" . $s . "   -->\n";
+  } else {
+    $s =~ s!^!## !mg;
+  }
+}
+
+
+=head1 LICENSE
+
+Copyright 2003 Wakaba <w@suika.fam.cx>.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+=cut
+
+## $Date: 2003/07/06 08:32:27 $
